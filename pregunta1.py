88cc7a6531a0067affc36e0822b3ad02212763f0
luciofondon98 <luciofondon98@gmail.com>
2020-06-09 16:32:31 -0400

    p1 datos de relaciones guardados

--- pregunta1.py
+++ pregunta1.py
@@ -1,36 +1,18 @@
-
-#1) ordenar los datos de cada relación y guardarlos en memoría
-#2) encontrar los atributos en común que tienen las 2 relaciones (A-B / B-C)
-#3) comparar las tuplas y buscar las que tengan el atributo igual
-n_R = int(input())
+#Guardando datos de relacion R
+n_atributos_R = int(input())
 atributos_R = list(input().rstrip().split(" "))
-print(atributos_R)
-    
+n_tuplas_R = int(input())
+tuplas_R = []
+for i in range(n_tuplas_R):
+        tuplas_R.append(list(input().rstrip().split(" ")))
 
-
-#"0 1" -> (0,1)
-
-# 2
-# 0 1 -> (0,1)
-# 7
-# 1 2
-# 2 4
-# 4 1
-# 5 2 ---> [[1,2],[2,4]...]
-# 4 5
-# 3 4
-# 3 5
-# 2
-# 1 2
-# 3
-# 3 1
-# 2 3
-# 5 3
-# 3
-# 2 3 4
-# 2
-# 2 5 6
-# 6 7 8
+#Guardando datos de relacion S
+n_atributos_S = int(input())
+atributos_S = list(input().rstrip().split(" "))
+n_tuplas_S = int(input())
+tuplas_S = []
+for i in range(n_tuplas_S):
+        tuplas_S.append(list(input().rstrip().split(" ")))
 
 def busqueda(pos_atr_R, pos_atr_S, lista_R, lista_S, atr_R, atr_S):
     lista_T = []
@@ -54,11 +36,7 @@ def similares(lista_R, lista_S, atr_R, atr_S, n_R, n_S):
                 comun.append(atr_R[i])
     print(comun)
 
-
-
-
-
-similares( [[1,2],[2,3]] , [[2,1,5],[2,3,10]] , [0,1] , [1,2,3] , 2 , 3)
+similares(tuplas_R,tuplas_S,atributos_R,atributos_S,n_atributos_R,n_atributos_S)
 #buscar cuando tienen + de 1 columna en comun
         
 
