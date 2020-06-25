cee029b9881b4881ca19d6a75d877f4a3dfa1aa4
Rodrigo Cayazaya <rodrigo.cayazaya@sansano.usm.cl>
2020-06-24 20:29:38 -0400

    p3 funciona, tiene muchos len Co-authored-by: jainaga <francisco.reyesja@sansano.usm.cl> Co-authored-by: Lucio Fondón R. <luciofondon98@gmail.com>

--- Pregunta 3/pregunta_3.1.py
+++ Pregunta 3/pregunta_3.1.py
@@ -1,15 +1,25 @@
-linea_1 = input().rstrip().split(" ")
-n = int(linea_1[0])
-k = int(linea_1[1])
+def crearMatriz():
+    linea_1 = input().rstrip().split(" ")
+    n = int(linea_1[0])
+    k = int(linea_1[1])
 
-matriz_1 = []
-matriz_2 = []
+    matriz_1 = []
+    matriz_2 = []
+
+    #guardamos valores primera matriz
+    for i in range(k * n):
+        fila = list(map(int, input().rstrip().split(" ")))
+        matriz_1.append(fila)
+
+    for i in range(n):
+        fila = list(map(int, input().rstrip().split(" ")))
+        matriz_2.append(fila)
+
+    return matriz_1,matriz_2,n,k
 
 
-
-
-def combinar(C1, C2, C3, C4): # combina cuatro matrices en una
-    p = len(C1)*2
+def combinar(C1, C2, C3, C4): # combina cuatro matrices en una NO USAR LEN
+    p = len(C1)*2 #no usar len
     matriz=[]
     for fila in range(p):       # creamos una matriz del doble de dimensiones y llenamos con ceros
         lista = []
@@ -24,16 +34,6 @@ def combinar(C1, C2, C3, C4): # combina cuatro matrices en una
                 matriz[i+len(C1)][j] = C3[i][j] #tercer cuadrante
                 matriz[i+len(C1)][j+len(C1)] = C4[i][j] #cuarto cuadrante
     return matriz
-
-
-#guardamos valores primera matriz
-for i in range(k * n):
-    fila = list(map(int, input().rstrip().split(" ")))
-    matriz_1.append(fila)
-
-for i in range(n):
-    fila = list(map(int, input().rstrip().split(" ")))
-    matriz_2.append(fila)
    
 def sum_matrices(m1,m2): #parametros: dos matrices y retorna la suma de las matrices 
     if type(m1) == int:
@@ -47,14 +47,15 @@ def sum_matrices(m1,m2): #parametros: dos matrices y retorna la suma de las matr
             suma.append(lista)
     return suma
 
-def res_matrices(m1,m2): #parametros: dos matrices y retorna la resta de las matrices ok
+def res_matrices(m1,m2): #parametros: dos matrices y retorna la resta de las matrices 
     if type(m1) == int:
         resta = m1 - m2
     else:
+        xd = len(m1[0])
         resta = []
         for i in range(len(m1)):
             lista = []
-            for j in range(len(m1[0])):
+            for j in range(xd):
                 lista.append(m1[i][j] - m2[i][j])
             resta.append(lista)
     return resta
@@ -67,7 +68,7 @@ def dividir_matrices(matriz,n):     #ok
     D=[]
     for fila in matriz:
         if(i<(n/2)):
-            A.append(fila[:int((n/2))])
+            A.append(fila[:int((n/2))]) #puede ser de más
             B.append(fila[int((n/2)):])
             i+=1
         else:
@@ -98,13 +99,13 @@ def strassen(m1,m2,n):
         C3 = sum_matrices(P2,P4)
         C4 = sum_matrices(res_matrices(sum_matrices(P1, P3), P2), P6)
 
-        c = combinar(C1,C2,C3,C4)       #los parametros son las matrices a combinar en una del doble de sus dimensiones
+        c = combinar(C1,C2,C3,C4)       #los parametros son las matrices a combinar en una matriz del doble de sus dimensiones
         
         return c
 
 
 
-
-M = strassen(matriz_1,matriz_2,n)
+M1,M2,n,k = crearMatriz()
+M = strassen(M1,M2,n)
 
 print(M)
